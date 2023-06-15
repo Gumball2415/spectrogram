de3e0d5537276b25ff32ea3703ef56677581a34b
Persune <gumball2415@gmail.com>
2023-06-16 02:07:51 +0800

    Use uniform noise instead of straight ifft
    thanks Hibiki for the tip on random phase!

--- spectrogram.py
+++ spectrogram.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python
 import numpy as np
-import matplotlib.image as mpimg
+from PIL import Image
 import wave
 from array import array
 
+length = 7  # seconds
+samplerate = 96000
 
 def make_wav(image_filename):
     """ Make a WAV file having a spectrogram resembling an image """
     # Load image
-    image = mpimg.imread(image_filename)
+    image = Image.open(image_filename)
+    w_img, h_img = image.width, image.height
+    image = image.resize((int(length * samplerate / (h_img * 2)), h_img))
     image = np.sum(image, axis = 2).T[:, ::-1]
-    image = image**3 # ???
     w, h = image.shape
 
-    # Fourier transform, normalize, remove DC bias
-    data = np.fft.irfft(image, h*2, axis=1).reshape((w*h*2))
+    max32 = 2**31-1
+    data = image
+    # phase randomization
+    rng = np.random.default_rng()
+    noise = rng.standard_normal(size=data.shape) * 2*np.pi
+    data = (np.real(data) * np.sin(noise)) + (np.imag(data) * np.cos(noise))
+    # Fourier transform
+    data = np.fft.irfft(data, h*2, axis=1)
+    # normalization and DC offset fix
+    data = np.real(data) + np.imag(data)
     data -= np.average(data)
-    data *= (2**15-1.)/np.amax(data)
-    data = array("h", np.int_(data)).tostring()
+    data *= (np.float64(max32))/np.amax(data)
+    # value cast
+    data = np.int32(data)
 
     # Write to disk
-    output_file = wave.open(image_filename+".wav", "w")
-    output_file.setparams((1, 2, 44100, 0, "NONE", "not compressed"))
+    output_file = wave.open(image_filename+".wav", "wb")
+    output_file.setparams((1, 4, samplerate, 0, "NONE", "not compressed"))
     output_file.writeframes(data)
     output_file.close()
-    print "Wrote %s.wav" % image_filename
+    print("Wrote %s.wav" % image_filename)
 
 
 if __name__ == "__main__":
